797ab94f4005db17a169e0116055dc2a5d7ea622
Joe Jobes <jmrjobes@gmail.com>
2018-05-29 16:37:16 -0600

    Add exercises spec
    Issue #27

---
+++ test/exercises.spec.js
@@ -0,0 +1,53 @@
+const chai = require('chai');
+const expect = chai.expect();
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+const request = require('supertest');
+
+chai.use(chaiHttp);
+
+describe('Authentication Functionality', () => {
+
+  const userCredentials = {
+    email: 'jmrjobes@yahmail.biznis',
+    username: 'joe',
+    password: 'password'
+  }
+
+  var authenticatedUser = request.agent(app);
+
+  before((done) => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch((error) => {
+      throw error;
+    })
+    .done();
+  })
+
+  beforeEach((done) => {
+    database.seed.run()
+    authenticatedUser
+      .post('/login')
+      .send(userCredentials)
+      .end((err, response) => {
+        expect(response.statusCode).to.equal(302)
+        expect('Location', '/dashboard')
+        done();
+      })
+  })
+
+  describe("GET /exercises", () => {
+    it("should return all exercises", () => {
+      return chai.request(app)
+        .get("/exercises")
+        .then((response) => {
+          response.should.have.status(200)
+          response.text.includes("ab crunch machine")
+          response.text.includes("alternating renegade row")
+        })
+    })
+  })
+
+})
