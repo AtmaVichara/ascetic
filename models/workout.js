f00665fc713d1b8810bad9740f5a3b64fd547ba1
Joe Jobes <jmrjobes@gmail.com>
2018-05-31 12:50:23 -0600

    Add #allUsersWorkouts method to Workout model
    Add allUsersWorkouts method that returns all the workouts associated with a user, and the exercises associated with each workout in the object returned

--- models/workout.js
+++ models/workout.js
@@ -4,6 +4,27 @@ const database        = require('knex')(configuration);
 
 class Workout {
 
+  static allUserWorkouts(userId) {
+    return database.raw(`SELECT workouts.*,
+      (
+        SELECT json_agg(json_build_object('name', name, 'sets', workout_exercises.sets, 'reps', workout_exercises.reps))
+        FROM exercises
+        INNER JOIN workout_exercises ON exercises.id = workout_exercises.exercise_id
+        WHERE workout_exercises.workout_id = workouts.id
+      ) as exercises
+      FROM workouts
+      INNER JOIN workout_exercises ON workouts.id = workout_exercises.workout_id
+      WHERE workouts.user_id = ?
+      GROUP BY workouts.id
+      ORDER BY workouts.id
+    `, userId)
+    .then((workouts) => {
+      return workouts.rows
+    })
+  }
+
+
+
   static create(workoutName, userId) {
     return database("workouts")
       .insert({
