db763dbd647510bc454f979190bc14576a3d0ac5
Joe Jobes <jmrjobes@gmail.com>
2018-06-03 06:57:43 -0600

    Add WorkoutExercises PATCH route and methods
    Add #findByWorkoutAndExerciseId method
    Add #findAllByWorkoutId method
    
    Issue [#44, #49]

--- models/workout-exercises.js
+++ models/workout-exercises.js
@@ -8,6 +8,20 @@ class WorkoutExercise {
     return database("workout_exercises")
       .insert(attributes)
       .returning("*")
+      .catch((error) => console.error({error}))
+  }
+
+  static findByWorkoutAndExerciseId(workoutId, exerciseId) {
+    return database("workout_exercises")
+      .select("*")
+      .where({
+        workout_id: workoutId,
+        exercise_id: exerciseId
+      })
+      .then((workoutExercise) => {
+        return workoutExercise[0]
+      })
+      .catch((error) => console.error({error}))
   }
 
   static async createAll(exercises, workout) {
@@ -16,6 +30,31 @@ class WorkoutExercise {
       return await this.create(attributes)
     })
     return await Promise.all(workoutExercises)
+  }
+
+  static findAllByWorkoutId(workoutId) {
+    return database.raw(`
+      SELECT reps, sets, exercises.name AS exercise
+      FROM workout_exercises
+      INNER JOIN exercises ON workout_exercises.exercise_id = exercises.id
+      WHERE workout_exercises.workout_id = ?
+      GROUP BY workout_exercises.id
+      `, workoutId)
+      .then((workoutExercises) => {
+        return workoutExercises.rows
+      })
+      .catch((error) => console.error({error}))
+  }
+
+  static updateSetsReps(attributes, workoutExerciseId) {
+    return database("workout_exercises")
+      .where("id", workoutExerciseId)
+      .update(attributes)
+      .returning("*")
+      .then((workoutExercise) => {
+        return workoutExercise[0]
+      })
+      .catch((error) => console.error({error}))
   }
 
 }
