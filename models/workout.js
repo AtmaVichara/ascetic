b2a2d6b897e119fcc261b3d3de40cc015cbcbd7a
Joe Jobes <jmrjobes@gmail.com>
2018-06-03 06:59:30 -0600

    Add #findLastByUserId to Workout model

--- models/workout.js
+++ models/workout.js
@@ -5,13 +5,18 @@ const database        = require('knex')(configuration);
 class Workout {
 
   static allUserWorkouts(userId) {
-    return database.raw(`SELECT workouts.*,
-      (
-        SELECT json_agg(json_build_object('name', name, 'sets', workout_exercises.sets, 'reps', workout_exercises.reps))
+    return database.raw(`
+      SELECT workouts.*,(
+        SELECT json_agg(
+          json_build_object(
+            'id', exercises.id,
+            'name', name,
+            'sets', workout_exercises.sets,
+            'reps', workout_exercises.reps))
         FROM exercises
         INNER JOIN workout_exercises ON exercises.id = workout_exercises.exercise_id
         WHERE workout_exercises.workout_id = workouts.id
-      ) as exercises
+      ) AS exercises
       FROM workouts
       INNER JOIN workout_exercises ON workouts.id = workout_exercises.workout_id
       WHERE workouts.user_id = ?
@@ -21,9 +26,19 @@ class Workout {
     .then((workouts) => {
       return workouts.rows
     })
+    .catch((error) => console.error({error}))
   }
 
-
+  static findLastByUserId(userId) {
+    return database("workouts")
+      .select("*")
+      .where("user_id", userId)
+      .orderBy("id", "asc")
+      .then((workout) => {
+        return workout[0]
+      })
+      .catch((error) => console.error({error}))
+  }
 
   static create(workoutName, userId) {
     return database("workouts")
