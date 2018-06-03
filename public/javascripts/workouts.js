c82f54807171e8e4ae074798d9146a03f7e7998d
Joe Jobes <jmrjobes@gmail.com>
2018-06-03 06:58:47 -0600

    Add reps and sets fetch update
    Add javascript for setting reps and sets in the workouts index
    Add fetch call to PATCH /workouts/:workout_id/exercises/:exercise_id
    
    Issue #49

--- public/javascripts/workouts.js
+++ public/javascripts/workouts.js
@@ -1,8 +1,37 @@
 $(document).ready(() => {
-  $('#workoutHeader').on('click', () => {
-    $('.exerciseShow').toggle(() => {
-      $(this).show('fast', 'swing');
-    })
-    event.preventDefault();
+  $("button").click((event) => {
+    var workoutInfo = $(event.currentTarget).attr("id")
+    var sets = $(`input#${workoutInfo}-sets`).val()
+    var reps = $(`input#${workoutInfo}-reps`).val()
+    let exerciseId = workoutInfo.split("-")[1]
+    let workoutId = workoutInfo.split("-")[3]
+    updateSetsReps(sets, reps, workoutId, exerciseId)
   })
 })
+
+
+const updateSetsReps = (exSets, exReps, workoutId, exerciseId) => {
+
+  fetch(`/workouts/${workoutId}/exercises/${exerciseId}`, {
+    body: JSON.stringify({
+      sets: exSets,
+      reps: exReps,
+    }),
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    method: "PATCH"
+  })
+  .then((response) => response.json())
+  .then((response) => {
+    if (response.hasOwnProperty("success")) {
+      alert(response.success)
+    } else if (response.hasOwnProperty("error")){
+      alert(response.error)
+    } else {
+      alert("Reps and Sets Were Not Saved")
+    }
+  })
+  .catch((error) => console.error({error}))
+  
+}
