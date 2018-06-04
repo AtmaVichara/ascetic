10b04c7b9acb8ea86857e96c3a0d6663a5a32f95
Joe Jobes <jmrjobes@gmail.com>
2018-06-04 13:56:20 -0600

    Add session table migration
    Add session table for storage
    
    Issue #57

---
+++ db/migrations/20180604135040_create-session-table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  let createQuery = `CREATE TABLE "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+  	"sess" json NOT NULL,
+  	"expire" timestamp(6) NOT NULL
+  )
+  WITH (OIDS=FALSE);
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;`
+  return knex.raw(createQuery)
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('session')
+};
