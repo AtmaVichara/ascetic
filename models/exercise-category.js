26ddb81168c89cc219e525af935464347a40d4d7
Joe Jobes <jmrjobes@gmail.com>
2018-06-05 08:38:51 -0600

    Update ExerciseCategory model
    Add allWithExercises() method
    Delete findByName() (It was not being used
    
    Issue #72 #82

--- models/exercise-category.js
+++ models/exercise-category.js
@@ -4,14 +4,20 @@ const database        = require('knex')(configuration);
 
 class ExerciseCategory {
 
-  static findByName(name) {
-    return database('exercise_categories')
-      .select('*')
-      .where('name', name)
-      .then((exerciseCategory) => {
-        return exerciseCategory[0]
-      })
-      .catch((error) => console.error({error}))
+  static allWithExercises() {
+    return database.raw(`SELECT exercise_categories.*, (
+        SELECT json_agg(name)
+        FROM exercises
+        WHERE exercises.category_id = exercise_categories.id
+      ) AS exercises
+      FROM exercise_categories
+      INNER JOIN exercises ON exercise_categories.id = exercises.category_id
+      GROUP BY exercise_categories.id
+    `)
+    .then((exerciseCategory) => {
+      return exerciseCategory.rows
+    })
+    .catch((error) => console.error({error}))
   }
 
 }
