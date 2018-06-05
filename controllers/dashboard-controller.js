7de0dfe25cfa128c53b77d03058e10fc40d89767
Joe Jobes <jmrjobes@gmail.com>
2018-06-05 10:02:29 -0600

    Add spacing between variables for readability in controllers

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -5,8 +5,9 @@ const Workout = require('../models/workout')
 class DashboardController {
 
   static async index(req, res, next) {
-    let userId = req.session.user.id
+    let userId       = req.session.user.id
     let userWorkouts = await Workout.allUserWorkouts(userId)
+
     res.render('dashboard', {workouts: userWorkouts})
   }
 
